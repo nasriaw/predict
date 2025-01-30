40b09fab0a9c73d2b524e191f4a6f90638a80110
nasriaw <nasriaw@gmail.com>
2025-01-30 17:38:05 +0700

    Update functions_statistics.py

--- functions_statistics.py
+++ functions_statistics.py
@@ -39,9 +39,9 @@ def open_file():
         #st.write("data head : ")
         #st.write(df.head()) 
     return df
-
+df=open_file()
 def descriptive():
-    df=open_file()
+    #df=open_file()
     #st.write(f"dimensi data: {df.shape}")
     #st.write("Data Head : ")
     #st.write(df.head())
