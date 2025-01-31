24abe845e0e492459e621a4da806cbe20233f2cc
nasriaw <nasriaw@gmail.com>
2025-01-31 14:26:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ menu=pd.DataFrame(features)
 #st.write(m,n)
 #st.sidebar.image("logo_stiei.jpg", use_column_width=False)
 st.sidebar.markdown('<h5 style="color: white;"> Author: Nasri </h5>', unsafe_allow_html=True)
+st.sidebar.markdown('<h5 style="color: white;"> Author: email: nasri@stieimlg.ac.id </h5>', unsafe_allow_html=True)
 st.sidebar.markdown('<h1 style="color: yellow;">Analisis Statistik Regresi Linear</h1>', unsafe_allow_html=True)
 
 model_analisis = st.sidebar.radio('Pilih Analisis Statistik', menu)
